2ead3667b6f9b6cb1fbe7f4198e68ad0d5f8f833
frede122 <fredemanduoliveira@hotmail.com>
2020-06-24 20:14:34 -0300

    feat(design): Add carousel, posts e hq-carousel :art:

---
+++ Design/Portal-Geek/page/carousel.js
@@ -0,0 +1,55 @@
+
+var item = document.getElementById("carousel").getElementsByClassName('item');
+var current = 0;
+var lock = false;
+const totalSlid = item.length;
+
+function addAllCarouselItem(value){ 
+    for(i = 0; i < item.length; i++){
+        item[i].classList.add(value);
+    }
+}
+function removeAllCarouselItem(value){
+    for(i = 0; i < item.length; i++){
+        item[i].classList.remove(value);
+    }
+}
+
+function active(element){
+    element.style.zIndex = 990;
+    addAllCarouselItem("blur");
+    element.classList.remove("blur");
+    element.classList.add("active");
+    // window.setTimeout(element.style.zIndex = 3, 1000);
+}
+function desactive(element, time){
+    
+    removeAllCarouselItem("blur");
+    element.classList.remove("active");
+    // element.style.zIndex = 0;
+    window.setTimeout(() => {element.style.zIndex = 0 }, time);
+}
+
+function playCarousel(time, pause, start, aleator = false){
+    if(start)
+        window.setInterval(() =>{
+            if(!lock){
+                cur = current;
+                if(aleator)
+                    cur = Math.floor(Math.random() * item.length);
+                active(item[cur]);
+                window.setTimeout(() => {
+                    desactive(item[cur], pause);
+                }, pause); 
+                current >= totalSlid -1 ? this.current = 0  : this.current++;
+            }
+        }, time);
+}
+function carMouseOver(){
+    lock = true;
+    removeAllCarouselItem("active");
+}
+function carMouseOut(){ lock = false; }
+
+playCarousel(7500, 2500, true, true);
+
